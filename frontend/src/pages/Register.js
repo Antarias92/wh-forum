84bc7d3c31b3437bb90766ada5788ce5ad37b2dc
Anthony Arias <aarias92@gmail.com>
2025-04-26 16:56:40 -0500

    created registration and login forms and applied styling

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -2,24 +2,63 @@ import React from "react";
 
 const Register = () => {
     return(
-        <div>
-            <div>
+        <div className="register_content justify-content-center align-items-center d-flex shadow-lg">
+            {/*-----Register Section-----*/}
+            <div className="col-md-6 d-flex justify-content-center">
                 <form>
-                    <div>
+                    <div className="header-text mb-4">
+                        <h2>Register</h2>
+                    </div>
+                    <div className="input_group mb-3">
                         <label htmlFor="username">Username</label>
-                        <input type="text" placeholder="Enter Username"/>
+                        <input type="text" className="form-control" placeholder="Enter Username" required/>
                     </div>
-                    <div>
+                    <div className="input_group mb-3">
+                        <label htmlFor="username">Email</label>
+                        <input type="email" className="form-control" placeholder="Enter Email" required/>
+                    </div>
+                    <div className="input_group mb-3">
                         <label htmlFor="username">Password</label>
-                        <input type="text" placeholder="Enter Password"/>
+                        <input type="text" className="form-control" placeholder="Enter Password" required/>
                     </div>
-                    <button>Submit</button>
-                </form>
-                <div>
+                    <button className="btn btn-primary">Submit</button>
+                    <div>
                     <p>Already have an account</p>
                     <a href="">Login</a>
                 </div>
+                </form>
             </div>
+
+             {/*-----Login Section-----*/}
+             <div className="col-md-6 right_box">
+                <form>
+                    <div className="header-text mb-4">
+                        <h2>Login</h2>
+                    </div>
+                    <div className="input_group mb-3">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" placeholder="Enter Username" required/>
+                    </div>
+                    <div className="input_group mb-3">
+                        <label htmlFor="username">Password</label>
+                        <input type="text" className="form-control" placeholder="Enter Password" required/>
+                    </div>
+                    <button className="btn btn-primary">Submit</button>
+                 
+                </form>
+            </div>
+
+            {/*-----Switch Sections-----*/}
+            <div className="switch_content">
+                <div className="switch">
+                    <div className="switch_section switch_left">
+                        <h2>Welcome Back</h2>
+                        
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
